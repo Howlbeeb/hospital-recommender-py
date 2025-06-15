64fa76b230d418b312cc5fc5880efa93949ed719
Habeeblahi Balogun <balogunhabeeb4@gmail.com>
2025-06-15 21:37:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from hospital_recommender import recommend_hospitals
 import pandas as pd
